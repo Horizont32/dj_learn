0767e62bb63740cb7326614277844798ec6af29e
Artem Golovachev <artbryansk94@gmail.com>
2023-03-03 00:59:41 +0300

    Added models

--- learn/sub1/models.py
+++ learn/sub1/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
+
 # Create your models here.
+
+class User(models.Model):
+    user_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    nickname = models.CharField(max_length=100)
+
+
+class Message(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.CharField(max_length=255)
+    date = models.DateField(auto_now_add=True)
+
+
+class Event(models.Model):
+    event_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=50)
+    summ = models.IntegerField()
+    payer = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Event_Ower(models.Model):
+    event_id = models.ForeignKey(Event, on_delete=models.CASCADE)
+    ower_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    owed_summ = models.IntegerField()
